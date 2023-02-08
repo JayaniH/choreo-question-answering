96069679af084d33c56e0ef408ea2a4285d2c62a
JayaniH <jayani.hewa77@gmail.com>
2023-02-08 09:38:08 +0530

    add error logs

--- service/question_answering_service.py
+++ service/question_answering_service.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import request
 from service.question_answering import answer_query_with_context
 from flask import current_app as app
@@ -6,10 +8,18 @@ from flask import current_app as app
 @app.route('/generate_answer', methods=['POST'])
 def generate_answer():
 
-    json_body = request.json
-    question = json_body.get('question')
-    answer = answer_query_with_context(question)
+    try:
+        json_body = request.json
+        question = json_body.get('question')
 
-    return {
-        'answer': answer
-    }
+        answer = answer_query_with_context(question)
+
+        return {
+            'answer': answer
+        }
+
+    except Exception as e:
+        logging.error("Request is not properly formatted: " + str(e), exc_info=True)
+        return {
+            'answer': ""
+        }
