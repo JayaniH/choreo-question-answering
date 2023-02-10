5d3c593c7ee66378ad504fd6fcbe1456376cd529
JayaniH <jayani.hewa77@gmail.com>
2023-02-10 14:47:52 +0530

    write questions and answers to a google sheet

--- service/constants.py
+++ service/constants.py
@@ -1,3 +1,5 @@
+import os.path
+
 COMPLETIONS_MODEL = "text-davinci-003"
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
@@ -10,3 +12,11 @@ COMPLETIONS_API_PARAMS = {
     "max_tokens": 500,
     "model": COMPLETIONS_MODEL,
 }
+
+DATA_DIR = "data"
+DOCUMENT_LIBRARY_PATH = os.path.join(DATA_DIR, "choreo_docs_library_with_tokens.csv")
+DOCUMENT_EMBEDDINGS_PATH = os.path.join(DATA_DIR, "choreo_document_embeddings.pkl")
+QUESTION_EMBEDDINGS_PATH = os.path.join(DATA_DIR, "choreo_question_embeddings.pkl")
+QUESTIONS_AND_ANSWERS_PATH = os.path.join(DATA_DIR, "questions_and_answers.csv")
+
+GS_CREDENTIALS_PATH = "/config/gs_credentials.json"
